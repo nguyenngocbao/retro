bde37859a88668eaf0c1f1f71ee91e4e17640363
admin <user1@user1noMacBook-Pro.local>
2023-11-11 10:26:10 +0700

    Update loading

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -8,22 +8,32 @@ export const Logo = () => {
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    // setTimeout(() => {
-    ref.current.style.display = 'none';
-    // }, 4000);
+    setTimeout(() => {
+      ref.current.style.display = 'none';
+    }, 2000);
   }, []);
 
   return (
     <div ref={ref}>
+      <svg
+        className="logo-spread max-w-screen fixed left-[50%] top-[50%] z-[-1] h-[200px] w-[200px] translate-x-[-50%] translate-y-[-51%] overflow-visible"
+        width="200"
+        height="200"
+      >
+        <circle className="logo-loading" cx="50%" cy="50%" r="50%" />
+      </svg>
       <Image
         unoptimized
         alt="Retrocraft"
-        className="logo-spread max-w-screen fixed left-[50%] top-[50%] z-[-1] w-[400px] translate-x-[-50%] translate-y-[-50%]"
+        className="max-w-screen fixed left-[50%] top-[50%] z-[-1] w-[300px] translate-x-[-50%] translate-y-[-50%]"
         src="/logo_retrocraft.png"
         width={1024}
         height={1024}
         priority
       />
+      <p className="fixed left-[50%] top-[calc(50%_+_150px)] z-[-1] translate-x-[-45%] translate-y-[-50%]">
+        Loading...
+      </p>
     </div>
   );
 };
